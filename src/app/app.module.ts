9eee1ae851e636b3818fbc5f604102133e3ef12c
fmancevich <79118642+fmancevich@users.noreply.github.com>
2021-08-11 13:18:00 -0300

    Primeros pasos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactListComponent } from './components/main/contact-list/contact-list.component';
+import { ContactDataComponent } from './components/main/contact-data/contact-data.component';
+import { ListSearchComponent } from './components/main/contact-list/list-search/list-search.component';
+import { ListResultComponent } from './components/main/contact-list/list-result/list-result.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    HeaderComponent,
+    ContactListComponent,
+    ContactDataComponent,
+    ListSearchComponent,
+    ListResultComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/main/contact-list/list-result/list-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-result',
+  templateUrl: './list-result.component.html',
+  styleUrls: ['./list-result.component.css']
+})
+export class ListResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
